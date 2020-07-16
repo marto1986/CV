ce856eb99e58ddb49912201f2982dce6fb530e7b
Martin <martinmatias@outlook.com>
2020-07-16 11:09:47 -0300

    se crea el controlador PDF

---
+++ CV.Web/Controllers/PDFController.cs
@@ -0,0 +1,21 @@
+﻿using CV.Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http.Formatting;
+
+namespace CV.Web.Controllers
+{
+    public class PDFController : Controller
+    {
+        // GET: PDF
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
